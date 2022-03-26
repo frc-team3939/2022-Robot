4728595f071b8a85269d2c5bada9b146a2e322c1
aetzler25 <aetzler@vt.edu>
2022-03-25 21:38:41 -0400

    Adding Back in debouncing

--- src/main/java/frc/robot/util/DriveModule.java
+++ src/main/java/frc/robot/util/DriveModule.java
@@ -114,9 +114,9 @@ public class DriveModule {
 
 
   public void setAngle(double goal) {
-    //long curr = System.currentTimeMillis();
+    long curr = System.currentTimeMillis();
 
-    /*double diff = Math.abs(getAngle() - goal) % 2;
+    double diff = Math.abs(getAngle() - goal) % 2;
     if (diff > 1.0) {
       if (goal >= 0.0 && curr >= time_before_next_down) {
         offset_revs -= 1;
@@ -130,7 +130,7 @@ public class DriveModule {
     }
 
     goal += getOffset();
-    */
+    
     set_encoder_count = goal * countPerHalfRevolution;
     angleMotor.set(ControlMode.Position, set_encoder_count);
     
